2c4af2aeb8bf09f4f4874c7515c8b3b8c1daeeaf
SAIKOT-ROY <121844544+SAIKOT-ROY@users.noreply.github.com>
2024-10-14 01:45:00 +0600

    updeted the notFoundRoute.ts

--- src/app/middlewares/notFoundRoute.ts
+++ src/app/middlewares/notFoundRoute.ts
@@ -1,11 +1,23 @@
-import { NextFunction, Request, Response } from "express"
-import httpStatus from "http-status"
+// import { NextFunction, Request, RequestHandler, Response } from "express"
+// import httpStatus from "http-status"
 
-const notFound = (req: Request, res: Response, next: NextFunction) => {
-    return res.status(httpStatus.NOT_FOUND).json({
-        success:false,
+// const notFound: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
+//     return res.status(httpStatus.NOT_FOUND).json({
+//         success:false,
+//         message: "Not Found",
+//     })
+// }
+
+// export default notFound
+
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import httpStatus from "http-status";
+
+const notFound: RequestHandler = (req: Request, res: Response, next: NextFunction): void => {
+    res.status(httpStatus.NOT_FOUND).json({
+        success: false,
         message: "Not Found",
-    })
-}
+    });
+};
 
-export default notFound
+export default notFound;
